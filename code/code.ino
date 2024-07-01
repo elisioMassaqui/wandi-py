d1f5c1df43d2ea402ba962816981fb640069d375
Mr.Robot <elisiomassaqui8@gmail.com>
2024-07-01 21:20:26 +0100

    definitivo

--- code/code.ino
+++ code/code.ino
@@ -1,7 +1,21 @@
-void setup() {
-    // setup code here
-}
-
-void loop() {
-    // main code here
-}
+const int ledPin = 13; // Pino do LED
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600); // A taxa de transmissão deve corresponder à definida no Unity
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
+    {
+        char receivedChar = Serial.read();
+        if (receivedChar == '1')
+        {
+            digitalWrite(ledPin, HIGH); // Liga o LED
+            delay(1000); // Mantém o LED aceso por 1 segundo (opcional)
+            digitalWrite(ledPin, LOW); // Desliga o LED
+        }
+    }
+}
